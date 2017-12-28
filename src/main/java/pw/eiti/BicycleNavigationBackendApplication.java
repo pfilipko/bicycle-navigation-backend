f9a16dc2ff550f208c74e5f245f0d2aee441d403
Piotr Filipkowski <piotr.filipko@gmail.com>
2017-12-28 13:37:56 +0100

    SpringBoot app init

---
+++ src/main/java/pw/eiti/BicycleNavigationBackendApplication.java
@@ -0,0 +1,12 @@
+package pw.eiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicycleNavigationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicycleNavigationBackendApplication.class, args);
+	}
+}
